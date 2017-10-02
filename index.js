235e82078857ae387b2ce288ff0610d2e4492414
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-02 15:14:20 -0700

    Added listening app

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ var mongoose = require("mongoose");
 var app = express();
 
 mongoose.connect("mongodb://localhost/dnworld");
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+  res.render
+})
+
+app.listen(3000 function() {
+  console.log("Nomad draft listening on port 3000");
+});
