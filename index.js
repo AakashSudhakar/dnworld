90a50fa50c9c6f239384b032dbbe1b60e041d6e4
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-07 16:47:01 -0700

    Said hi to my friends

--- index.js
+++ index.js
@@ -92,55 +92,7 @@ app.put("/statuses/:id", function(req, res) {
     res.redirect("statuses/" + status._id);
   });
 });
-
-  /* $(document).ready(function() {
-      var arr = {
-         "unitedStatesOfAmerica":{
-             "Population": 300000000,
-             "GDP":123456789,
-             "Capital": "Washington, D.C."
-         },
-         "india":{
-             "Population":1300000000,
-             "GDP":55555555,
-             "Capital":"New Delhi"
-         },
-          "china":{
-              "Population":1400000000,
-              "GDP":121212121,
-              "Capital":"Beijing"
-         }
-       };
-
-       console.log(arr[0][1]); */
-
-      /*for (var key in json) {
-        if (json.hasOwnProperty(key) {
-          var reclaimer = json[key];
-          arr.push({
-            population: reclaimer.Population,
-            gdp: reclaimer.GDP,
-            capital: reclaimer.Capital
-          });
-        });
-      }
-      console.log(arr); */
-
-    // Attempting to access World Bank API data
-  /*  $(function() {
-      $.ajax({
-        contentType: "/application/json",
-        dataType: "json",
-        crossDomain: true,
-        url: "http://api.worldbank.org/countries?format=json",
-        success: function() {
-          console.log("SUCCESS");
-        },
-        error: function() {
-          console.log("ERROR");
-        }
-      });
-    }); */
+*/
 
 // App listening
 app.listen(3000, function() {
