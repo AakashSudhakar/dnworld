aec2b687cd941fbf3ba8b55ba5ecc7ff75677124
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-08 22:07:10 -0700

    see prior commit

--- index.js
+++ index.js
@@ -95,5 +95,4 @@ app.put("/statuses/:id", function(req, res) {
 // App listening to port 3000
 app.listen(3000, function() {
   console.log("Nomad draft listening on port 3000");
-  console.log("Hello Egon, how are you today?");
 });
