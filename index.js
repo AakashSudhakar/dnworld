08d2c2ba83b1dd31d4719d0bb411322137550f8e
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-08 22:03:50 -0700

    Added console log for testing

--- index.js
+++ index.js
@@ -95,4 +95,6 @@ app.put("/statuses/:id", function(req, res) {
 // App listening to port 3000
 app.listen(3000, function() {
   console.log("Nomad draft listening on port 3000");
+  console.log("Hello Egon, how are you today?");
 });
+
