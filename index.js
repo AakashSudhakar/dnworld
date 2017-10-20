65a068ea12d1a578893da0588787afbdfc647b09
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2017-10-20 05:26:50 -0700

    Accidentally deleted something I should not have

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ var Status = mongoose.model("Status", {
   city: String,
   description: String
 });
-
+var Address = mongoose.model("Address", {
+  country: String,
+  city: String
+});
 var bodyParser = require("body-parser");
 var chai = require("chai");
 var chaiHttp = require("chai-http");
